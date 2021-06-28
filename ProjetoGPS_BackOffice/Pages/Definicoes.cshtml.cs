1ad6f78d8078c8e8b2854677b281b74b78a80cbe
Milkenm <miguel.campos.johnny@gmail.com>
2021-06-28 17:25:48 +0100

    [BackOffice] Add tabs: Duvidas e Sugestoes

--- ProjetoGPS_BackOffice/Pages/Definicoes.cshtml.cs
+++ ProjetoGPS_BackOffice/Pages/Definicoes.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApplication1.Pages.Pages
+namespace ProjetoGPS_BackOffice.Pages.Pages
 {
     public class DefinicoesModel : PageModel
     {
