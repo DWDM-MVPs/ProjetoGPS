fd2802738e2555f1eff14996e093113726181456
Milkenm <miguel.campos.johnny@gmail.com>
2021-07-05 03:15:29 +0100

    Update

--- ProjetoGPS_BackOffice/Code.cs
+++ ProjetoGPS_BackOffice/Code.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 using Newtonsoft.Json;
 
@@ -6,11 +7,14 @@ using RestSharp;
 
 using System;
 using System.Linq;
+using System.Net;
 
 namespace ProjetoGPS_BackOffice.Pages
 {
 	public class Code
 	{
+		private bool checkLogin = true;
+
 		private readonly PageModel page;
 
 		public Code(PageModel pm, int appType)
@@ -20,10 +24,17 @@ namespace ProjetoGPS_BackOffice.Pages
 			this.GetApplications(appType);
 		}
 
+		public Code(PageModel pm)
+		{
+			page = pm;
+
+			checkLogin = false;
+		}
 
 
-		//public string ApiEndpoint = "https://papi-testapp2.herokuapp.com/api/";
-		public string ApiEndpoint = "https://localhost:44387/api/";
+
+		public string ApiEndpoint = "https://papi-testapp2.herokuapp.com/api/";
+		//public string ApiEndpoint = "https://localhost:44387/api/";
 
 
 
@@ -130,6 +141,66 @@ namespace ProjetoGPS_BackOffice.Pages
 			string body = JsonConvert.SerializeObject(app);
 			request.AddParameter("application/json", body, ParameterType.RequestBody);
 			client.Execute(request);
+		}
+
+
+
+
+
+
+
+
+
+
+		public bool Login(string username, string password)
+		{
+			var client = new RestClient($"https://localhost:44387/api/admins/login/{username}/{password}");
+			client.Timeout = -1;
+			var request = new RestRequest(Method.GET);
+			IRestResponse response = client.Execute(request);
+
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public string GetCookie(string key)
+		{
+			return page.Request.Cookies[key];
+		}
+
+		public void SetCookie(string key, string value, double? expireTime)
+		{
+			CookieOptions option = new CookieOptions();
+			if (expireTime.HasValue)
+			{
+				option.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+			}
+			else
+			{
+				option.Expires = DateTime.Now.AddMilliseconds(1);
+			}
+			page.Response.Cookies.Append(key, value, option);
+		}
+
+		public void DeleteCookie(string key)
+		{
+			page.Response.Cookies.Delete(key);
+		}
+
+		public void CheckLogin()
+		{
+			if (checkLogin == true && this.GetCookie("loggedIn") != "true")
+			{
+				this.Redirect("/Login");
+			}
+		}
+
+		public void Redirect(string url)
+		{
+			page.Response.Redirect(url);
 		}
 	}
 }
